e6f1ec9d4218203a60dcf98e50b7bbdb9765f131
Hannah Violet <hcbviolet@gmail.com>
2016-01-26 09:30:36 -0800

    Update address_book.rb and address_book_spec.rb with binary_search.

--- models/address_book.rb
+++ models/address_book.rb
@@ -31,10 +31,33 @@ class AddressBook
         csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
     #iterate over CSV::Table object's rows. Each row becomes a hash.
     #convert each row_hash to an entry using add_entry and extracting hash values
-    #and adding them to the AddressBook's entries. 
+    #and adding them to the AddressBook's entries.
         csv.each do |row|
           row_hash = row.to_hash
           add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
         end
+    end
+
+    def binary_search(name)
+      #start index of leftmost item in array in lower
+      #index of rightmost item in upper
+      lower = 0
+      upper = @entries.length - 1
+      #loop while lower <= upper
+      while lower <= upper
+      #find the middle index
+      mid = (lower + upper) / 2
+      mid_name = @entries[mid].name
+      #compare the search name to indexed name at middle point
+      #then diverge left or right based on alphabetical order
+      if name == mid_name
+        return @entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+      return nil
     end
 end
