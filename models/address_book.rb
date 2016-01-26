576b6f25112b48ba83c8eb6ddfa8b05e0845f1f1
Hannah Violet <hcbviolet@gmail.com>
2016-01-25 18:20:57 -0800

    Modified address_book.rb and address_book_spec.rb to import csv files. Added entries.csv for testing.

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,18 +1,21 @@
 # tell ruby to load library entry.rb relative to address_book.rb's file path
 require_relative 'entry'
+require "csv"
 
 class AddressBook
     attr_accessor :entries
-    
+
     def initialize
         @entries = []
     end
-    
+
     def add_entry(name, phone_number, email)
         # create a variable to store the insertion 'index'
         index = 0
         @entries.each do |entry|
-            #compare name with the name of the current entry. if it lexographically proceeds entry.name, we've found the index to insert at. otherwise we increment index and continue comparing with other entries.
+          #compare name with the name of the current entry. if it lexographically
+          #proceeds entry.name, we've found the index to insert at.
+          #otherwise we increment index and continue comparing with other entries.
             if name < entry.name
                 break
             end
@@ -20,5 +23,18 @@ class AddressBook
         end
         #insert a new entry into entries using the calculated index.
         @entries.insert(index, Entry.new(name, phone_number, email))
+    end
+
+    #Reads the csv file, parses it, saves as 'csv'
+    def import_from_csv(file_name)
+        csv_text = File.read(file_name)
+        csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+    #iterate over CSV::Table object's rows. Each row becomes a hash.
+    #convert each row_hash to an entry using add_entry and extracting hash values
+    #and adding them to the AddressBook's entries. 
+        csv.each do |row|
+          row_hash = row.to_hash
+          add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+        end
     end
 end
